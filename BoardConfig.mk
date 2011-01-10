3851614a093b35ba38a0992921ad1066a37b5517
bcnice20 <bcnice20@gmail.com>
2011-01-10 18:06:25 -0500

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,116 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/htc/speedy/BoardConfigVendor.mk
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := msm7x30
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+
+TARGET_BOOTLOADER_BOARD_NAME := speedy
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION      := VER_0_6_X
+BOARD_WLAN_DEVICE           := bcm4329
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcm4329.ko"
+WIFI_DRIVER_FW_STA_PATH     := "/system/etc/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_AP_PATH      := "/system/etc/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/firmware/fw_bcm4329.bin nvram_path=/proc/calibration"
+WIFI_DRIVER_MODULE_NAME     := "bcm4329"
+
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_PREBUILT_LIBAUDIO := true
+BOARD_CAMERA_USE_GETBUFFERINFO := true
+BOARD_OVERLAY_FORMAT_YCbCr_420_SP := true
+BOARD_USE_NEW_LIBRIL_HTC := true
+
+BOARD_KERNEL_CMDLINE := no_console_suspend=1
+BOARD_KERNEL_BASE := 0x04000000
+BOARD_KERNEL_PAGE_SIZE := 4096
+
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+BOARD_VENDOR_QCOM_AMSS_VERSION := 1200
+
+BOARD_EGL_CFG := device/htc/speedy/egl.cfg
+
+BOARD_USES_QCOM_LIBS := true
+
+BOARD_CAMERA_USE_GETBUFFERINFO := true
+
+BOARD_OVERLAY_FORMAT_YCbCr_420_SP := true
+
+BOARD_USE_NEW_LIBRIL_HTC := true
+
+BOARD_HAVE_FM_RADIO := true
+BOARD_USE_BROADCOM_FM_VOLUME_HACK := true
+
+BOARD_USE_FROYO_LIBCAMERA := true
+
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := speedy
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 1240
+
+# Use RGB565 surfaces until userspace drivers are upgraded
+BOARD_NO_RGBX_8888 := true
+
+# cat /proc/emmc
+# dev:        size     erasesize name
+#mmcblk0p17: 00040000 00000200 "misc"
+#mmcblk0p21: 0087f400 00000200 "recovery"
+#mmcblk0p22: 00400000 00000200 "boot"
+#mmcblk0p26: 1f7bf600 00000200 "system"
+#mmcblk0p29: 00140200 00000200 "local"
+#mmcblk0p28: 09a7fa00 00000200 "cache"
+#mmcblk0p27: 1bbffe00 00000200 "userdata"
+#mmcblk0p31: 01400200 00000200 "devlog"
+#mmcblk0p32: 00040000 00000200 "pdata"
+#mmcblk0p25: 00c00200 00000200 "wimax"
+#mmcblk0p30: 007ffa00 00000200 "udata_wimax"
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 435941376
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1394606080
+BOARD_BOOTIMAGE_PARTITION_SIZE := 4194304
+BOARD_FLASH_BLOCK_SIZE := 262144
+
+#TARGET_RECOVERY_UI_LIB := librecovery_ui_speedy librecovery_ui_htc
+
+TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
+TARGET_PREBUILT_KERNEL := device/htc/speedy/kernel
+
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_MISC_PARTITION := true
---
+++ speedy.mk
@@ -0,0 +1,164 @@
+#
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# This is the product configuration for a generic GSM passion,
+# not specialized for any geography.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+PRODUCT_COPY_FILES += \
+    device/htc/speedy/gps.conf:system/etc/gps.conf
+
+## (1) First, the most specific values, i.e. the aspects that are specific to GSM
+
+PRODUCT_COPY_FILES += \
+    device/htc/speedy/init.speedy.rc:root/init.speedy.rc \
+    device/htc/speedy/ueventd.speedy.rc:root/ueventd.speedy.rc
+
+## (2) Also get non-open-source GSM-specific aspects if available
+$(call inherit-product-if-exists, vendor/htc/speedy/speedy-vendor.mk)
+
+## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-sprint-us \
+    ro.com.google.clientidbase.am=android-sprint-us \
+    ro.com.google.clientidbase.vs=android-sprint-us \
+    ro.com.google.clientidbase.yt=android-sprint-us \
+    ro.com.google.clientidbase.gmm=android-sprint-us \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1 \
+    ro.com.google.gmsversion=2.2_r7 \
+    ro.setupwizard.enable_bypass=1 \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+DEVICE_PACKAGE_OVERLAYS += device/htc/speedy/overlay
+
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+# media config xml file
+PRODUCT_COPY_FILES += \
+    device/htc/speedy/media_profiles.xml:system/etc/media_profiles.xml
+
+PRODUCT_PACKAGES += \
+    librs_jni \
+    lights.speedy \
+    sensors.speedy \
+    gralloc.msm7x30 \
+    overlay.default \
+    gps.speedy \
+    libOmxCore \
+    libOmxVenc \
+    libOmxVdec
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    device/htc/speedy/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin \
+    device/htc/speedy/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
+    device/htc/speedy/keychars/speedy-keypad-ell.kcm.bin:system/usr/keychars/speedy-keypad-ell.kcm.bin \
+    device/htc/speedy/keychars/speedy-keypad-fra.kcm.bin:system/usr/keychars/speedy-keypad-fra.kcm.bin \
+    device/htc/speedy/keychars/speedy-keypad-ger.kcm.bin:system/usr/keychars/speedy-keypad-ger.kcm.bin \
+    device/htc/speedy/keychars/speedy-keypad-ita.kcm.bin:system/usr/keychars/speedy-keypad-ita.kcm.bin \
+    device/htc/speedy/keychars/speedy-keypad.kcm.bin:system/usr/keychars/speedy-keypad.kcm.bin \
+    device/htc/speedy/keychars/speedy-keypad-tur.kcm.bin:system/usr/keychars/speedy-keypad-tur.kcm.bin \
+    device/htc/speedy/keychars/speedy-keypad-wwe-bopomo.kcm.bin:system/usr/keychars/speedy-keypad-wwe-bopomo.kcm.bin \
+    device/htc/speedy/keychars/speedy-keypad-wwe.kcm.bin:system/usr/keychars/speedy-keypad-wwe.kcm.bin \
+    device/htc/speedy/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/speedy/keylayout/speedy-keypad-ara.kl:system/usr/keylayout/speedy-keypad-ara.kl \
+    device/htc/speedy/keylayout/speedy-keypad-ell.kl:system/usr/keylayout/speedy-keypad-ell.kl \
+    device/htc/speedy/keylayout/speedy-keypad-fra.kl:system/usr/keylayout/speedy-keypad-fra.kl \
+    device/htc/speedy/keylayout/speedy-keypad-ger.kl:system/usr/keylayout/speedy-keypad-ger.kl \
+    device/htc/speedy/keylayout/speedy-keypad-ita.kl:system/usr/keylayout/speedy-keypad-ita.kl \
+    device/htc/speedy/keylayout/speedy-keypad.kl:system/usr/keylayout/speedy-keypad.kl \
+    device/htc/speedy/keylayout/speedy-keypad-tur.kl:system/usr/keylayout/speedy-keypad-tur.kl \
+    device/htc/speedy/keylayout/speedy-keypad-wwe-bopomo.kl:system/usr/keylayout/speedy-keypad-wwe-bopomo.kl \
+    device/htc/speedy/keylayout/speedy-keypad-wwe.kl:system/usr/keylayout/speedy-keypad-wwe.kl
+
+# Firmware
+PRODUCT_COPY_FILES += \
+    device/htc/speedy/firmware/bcm4329.hcd:system/etc/firmware/bcm4329.hcd \
+    device/htc/speedy/firmware/default.acdb:system/etc/firmware/default.acdb \
+    device/htc/speedy/firmware/default_org.acdb:system/etc/firmware/default_org.acdb \
+    device/htc/speedy/firmware/fw_bcm4329.bin:system/etc/firmware/fw_bcm4329.bin \
+    device/htc/speedy/firmware/fw_bcm4329_apsta.bin:system/etc/firmware/fw_bcm4329_apsta.bin \
+    device/htc/speedy/firmware/vidc_720p_command_control.fw:system/etc/firmware/vidc_720p_command_control.fw \
+    device/htc/speedy/firmware/vidc_720p_h263_dec_mc.fw:system/etc/firmware/vidc_720p_h263_dec_mc.fw \
+    device/htc/speedy/firmware/vidc_720p_h264_dec_mc.fw:system/etc/firmware/vidc_720p_h264_dec_mc.fw \
+    device/htc/speedy/firmware/vidc_720p_h264_enc_mc.fw:system/etc/firmware/vidc_720p_h264_enc_mc.fw \
+    device/htc/speedy/firmware/vidc_720p_mp2_dec_mc.fw:system/etc/firmware/vidc_720p_mp2_dec_mc.fw \
+    device/htc/speedy/firmware/vidc_720p_mp4_dec_mc.fw:system/etc/firmware/vidc_720p_mp4_dec_mc.fw \
+    device/htc/speedy/firmware/vidc_720p_mp4_enc_mc.fw:system/etc/firmware/vidc_720p_mp4_enc_mc.fw \
+    device/htc/speedy/firmware/vidc_720p_vc1_dec_mc.fw:system/etc/firmware/vidc_720p_vc1_dec_mc.fw \
+    device/htc/speedy/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+    device/htc/speedy/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw
+
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# speedy uses high-density artwork where available
+PRODUCT_LOCALES += hdpi
+
+PRODUCT_COPY_FILES += \
+    device/htc/speedy/vold.fstab:system/etc/vold.fstab
+
+# Kernel modules
+#PRODUCT_COPY_FILES += \
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/htc/speedy/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+PRODUCT_COPY_FILES += \
+    device/htc/speedy/modules/bcm4329.ko:system/lib/modules/bcm4329.ko
+
+$(call inherit-product-if-exists, vendor/htc/speedy/speedy-vendor.mk)
+
+# media profiles and capabilities spec
+$(call inherit-product, device/htc/speedy/media_a1026.mk)
+
+# htc audio settings
+$(call inherit-product, device/htc/speedy/media_htcaudio.mk)
+
+# stuff common to all HTC phones
+$(call inherit-product, device/htc/common/common.mk)
+
+$(call inherit-product, build/target/product/full.mk)
+
+
+PRODUCT_NAME := htc_speedy
+PRODUCT_DEVICE := speedy
+PRODUCT_MODEL := HTC speedy
+PRODUCT_MANUFACTURER := HTC
+
